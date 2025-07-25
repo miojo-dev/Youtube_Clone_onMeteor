ea313c026b1adb528bf4c3b49f44a1be3f28a87a
miojo-dev <joaovitorhobus2@gmail.com>
2025-07-25 09:03:32 -0300

    Packages

--- tests/main.js
+++ tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("meteor-app", function () {
+describe("CloneTube", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "meteor-app");
+    assert.strictEqual(name, "CloneTube");
   });
 
   if (Meteor.isClient) {
